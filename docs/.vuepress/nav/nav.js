d8d2c62996c2b86d0501829a0b66128666321fc4
15262503512 <Kingtang327@163.com>
2021-02-01 14:16:40 +0800

    博客搭建

---
+++ docs/.vuepress/nav/nav.js
@@ -0,0 +1,52 @@
+module.exports = [
+  {
+    text: 'Java',
+    ariaLabel: 'Java',
+    items: [
+      {
+        text: 'Java',
+        items: [
+          {
+            text: '面试',
+            link: '/java/interview'
+          },
+          {
+            text: '源码',
+            link: '/java/source'
+          },
+          {
+            text: '资料',
+            link: '/java/resources'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    text: 'Python',
+    ariaLabel: 'Python',
+    items: [
+      {
+        text: 'Python',
+        items: [
+          {
+            text: '面试',
+            link: '/java/interview'
+          },
+          {
+            text: '源码',
+            link: '/java/source'
+          },
+          {
+            text: '资料',
+            link: '/java/resources'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    text: '关于我',
+    link: 'aboutme'
+  }
+]
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /hero.png
+actionText: 感受精彩 →
+actionLink: ../java/
+features:
+- title: Java
+  details: Java攻城狮一只
+- title: Python
+  details: 偶尔写写爬虫
+- title: Web
+  details: 也学习一点前端
+footer: MIT Licensed | Copyright © 2018-present Evan You
+---
