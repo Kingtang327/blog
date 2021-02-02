65d6f7721ad326194a65997427a2edf4d7c1eb6c
Kingtang <Kingtang327@163.com>
2021-02-02 18:13:03 +0800

    代码提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,68 @@
+// const { fs, path } = require('@vuepress/shared-utils')
+
+module.exports = ctx => ({
+    dest: '../../vuepress',
+    title: 'Java学习之路',
+    description: 'Java学习之路',
+    base: '/blog/',//这个地方要和你配置的那个地方一直
+    themeConfig: {
+        repo: '/Kingtang327/blog',
+        // editLinks: true,
+        docsDir: 'packages/docs/docs',
+        smoothScroll: true,
+        label: '简体中文',
+        selectText: '选择语言',
+        ariaLabel: '选择语言',
+        // editLinkText: '在 GitHub 上编辑此页',
+        lastUpdated: '上次更新',
+        nav: require('./nav/nav'),
+        sidebar: {
+            '/':'',
+            '/java/concurrent/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/docker/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/dubbo/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/jvm/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/mongodb/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/mq/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/mybatis/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/mysql/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/netty/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/redis/': getCommonSidebar('资源获取', '快速开始', '面试资料'),
+            '/java/spring/': getCommonSidebar('资源获取', '快速开始', '面试资料')
+        },
+        sidebarDepth: 2, // 侧边栏显示2级
+        extraWatchFiles: [
+            '.vuepress/nav/nav.js'
+        ]
+    }
+})
+
+function getCommonSidebar (groupA, groupB, groupC) {
+    return [
+        {
+            title: groupA,
+            collapsable: false,
+            sidebarDepth: 2,
+            children: [
+                'resources'
+            ]
+        },
+        {
+            title: groupB,
+            collapsable: false,
+            sidebarDepth: 2,
+            children: [
+                'getting-started'
+            ]
+        },
+        {
+            title: groupC,
+            collapsable: false,
+            sidebarDepth: 2,
+            children: [
+                'interview'
+            ]
+        }
+    ]
+}
+
---
+++ docs/.vuepress/dist/assets/js/5.66ae167c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{333:function(t,n,s){},364:function(t,n,s){"use strict";s(333)},426:function(t,n,s){"use strict";s.r(n);s(364);var e=s(42),i=Object(e.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"svg-container"},[this._t("default")],2)}),[],!1,null,null,null);n.default=i.exports}}]);
